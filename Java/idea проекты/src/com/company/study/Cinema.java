562cdc6f9047b6d4b5655230a73e461206805be2
Dima Densh <denshchikov2001@mail.ru>
2021-01-08 17:27:02 +0300

    add study

---
+++ Java/idea проекты/src/com/company/study/Cinema.java
@@ -0,0 +1,76 @@
+package com.company.study;
+
+import java.util.Scanner;
+
+public class Cinema {
+    public Cinema(){
+        Scanner scanner = new Scanner(System.in);
+
+        //количество рядов
+        System.out.println("Enter the number of rows:");
+        int countRows = scanner.nextInt();
+
+        //количество мест
+        System.out.println("Enter the number of seats in each row:");
+        int countSeats = scanner.nextInt();
+        countSeats++;
+        countRows++;
+        String[][] array = new String[countRows][countSeats];
+
+
+        for (int i = 0; i < countSeats; i++) {
+            array[0][i] = String.valueOf(i);
+        }
+
+        for (int i = 0; i < countRows; i++) {
+            array[i][0] = String.valueOf(i);
+        }
+
+
+
+        for (int i = 1; i < countRows; i++) {
+            for (int j = 1; j < countSeats; j++) {
+                array[i][j] = "S";
+            }
+        }
+
+        array[0][0] = " ";
+
+        System.out.println("Cinema:");
+        for (int i = 0; i < countRows; i++) {
+            for (int j = 0; j < countSeats; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.print('\n');
+        }
+
+        System.out.println("Enter a row number:");
+        int row = scanner.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int seat = scanner.nextInt();
+
+        array[row][seat] = "B";
+        System.out.println("Ticket price: $" + makeCost(countRows, countSeats, row));
+        System.out.println("Cinema:");
+        for (int i = 0; i < countRows; i++) {
+            for (int j = 0; j < countSeats; j++) {
+                System.out.print(array[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int makeCost(int countR, int countS, int r) {
+        countR--;
+        countS--;
+        int allRows = countR * countS;
+        int p = countR / 2;
+        if (allRows < 60){
+            return 10;
+        } else if (countR % 2 == 0){
+            return (r > p) ? 8:10;
+        } else {
+            return (r >= (p+1)) ? 8:10;
+        }
+    }
+}
---
+++ Java/idea проекты/src/com/company/study/WorkArray.java
@@ -0,0 +1,60 @@
+package com.company.study;
+
+public class WorkArray {
+    public static int sumHighN(int [] array, int N) {
+        int s = 0;
+        for (int n : array) {
+            if (n > N) {
+                s += n;
+            }
+        }
+        return s;
+    }
+
+    public static boolean checkTogether(int[]a, int n, int m){
+        for (int i = 0; i < a.length-1; i++) {
+            if ((a[i] == n) && (a[i+1] == m)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static boolean checkSymmetryMultiArray(int[][]array){
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if(array[i][j] != array[j][i]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static String findIndexMax(int[][]array){
+        int max = array[0][0];
+        int i_max = 0;
+        int j_max = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] > max) {
+                    max = array[i][j];
+                    i_max = i;
+                    j_max = j;
+                }
+            }
+        }
+        return i_max + " " + j_max;
+    }
+
+    public static boolean checkPalindrome(char[] chars){
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != chars[chars.length-1-i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
